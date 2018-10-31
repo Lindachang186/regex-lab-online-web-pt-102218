9106f206b55c242342c3e901fcdf687ad2d95cba
Linda Chang <lindachang186@gmail.com>
2018-10-31 18:49:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,10 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]/) && text.match(/[!?.]$/)
-     true
-  else false
-  end
+  text.match(/\A[A-Z]/) && text.match(/[!?.]$/)
 end
 
 def valid_phone_number?(phone)
