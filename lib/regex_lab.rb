8636e08b1fa1c40d2d5bcbe5d3c29196ac21fdb0
Linda Chang <lindachang186@gmail.com>
2018-10-31 18:43:04 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,12 +14,12 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]/) && text.match(/[!?.]/)
+  if text.match(/\A[A-Z]/) && text.match(/[!?.]$/)
      true
   else false
   end
 end
 
 def valid_phone_number?(phone)
-
+  if
 end
