5d3936d85e717ff05bfda723f850e6be10fd79e0
Linda Chang <lindachang186@gmail.com>
2018-10-31 18:54:38 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -21,5 +21,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/[0-9]{3}\s[0-9]{3}\s[0-9]{4}/)
+  if phone.match(/[0-9]{3}\s[0-9]{3}\s[0-9]{4}/)
+    return true
+  else return false
+  end
 end
