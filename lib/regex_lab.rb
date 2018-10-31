3138b327713244810e7bf58dd3203ac808ca9920
Linda Chang <lindachang186@gmail.com>
2018-10-31 18:43:58 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,7 +14,7 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/\A[A-Z]/) && text.match(/[!?.]$/)
+  if text.match(/\A[A-Z]/) && text.match(/[!?.]\z/)
      true
   else false
   end
